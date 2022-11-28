f1e14f10f67766e8d924aa4b5268ae3663e07df7
kkordan <94549547+kkordan@users.noreply.github.com>
2022-11-28 11:51:01 +0300

    Add files via upload

---
+++ 0.pas
@@ -0,0 +1,14 @@
+﻿uses GraphABC;
+begin
+SetPenColor(clblue);
+MoveTo (50, 50);
+LineTo (100, 50);
+LineTo (100, 100);
+LineTo (50, 100);
+LineTo (50, 50);
+var a:=sqrt(3)/2;
+MoveTo (200, 0);
+LineTo (260,100);
+LineTo (140, 100);
+LineTo (200, 0);
+end.
